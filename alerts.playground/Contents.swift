b8ae7f3acf4075edceea0aec6fa1c5a765de8260
wigging <wigging@me.com>
2016-09-07 23:49:03 -0400

    native swift approach

--- alerts.playground/Contents.swift
+++ alerts.playground/Contents.swift
@@ -1,12 +1,19 @@
-//: Alerts Playground
+/*
+
+Alerts playground for parsing weather alerts from the Weather Underground API.
+
+See the alerts.json file located in the Resources folder of this Playground
+for a local copy of the json data.
+
+Weather Underground API documentation:
+https://www.wunderground.com/weather/api/d/docs
+
+*/
 
 import UIKit
 
-/*
- 
- Model
- 
-*/
+// Model
+// -----------------------------------------------------------------------------
 
 struct Alert {
     let type: String
@@ -16,29 +23,32 @@ struct Alert {
     let message: String
 }
 
-/*
- 
- Parser
-
-*/
+// Parser
+// ----------------------------------------------------------------------
 
 class AlertsParser {
     
-    func alertsFrom(data: NSData?) -> [Alert]? {
-        
-        guard let data = data else { return nil }
-        let json = JSON(data: data)
+    /**
+     Parse alerts returned as json data from Weather Underground API.
+     - Parameter json: Dictionary representing json data.
+     - Returns: Array of alert structs.
+     */
+    
+    func alertsFrom(json: [String: AnyObject]?) -> [Alert]? {
         
         var alerts = [Alert]()
+
+        guard let alertsArray = json?["alerts"] as? [[String: AnyObject]] else { return nil }
         
-        for (_, item):(String, JSON) in json["alerts"] {
-            guard let type = item["type"].string else { return nil }
-            guard let date = item["date"].string else { return nil }
-            guard let descr = item["description"].string else { return nil }
-            guard let exp = item["expires"].string else { return nil }
-            guard let mess = item["message"].string else { return nil }
-            let al = Alert(type: type, date: date, description: descr, expires: exp, message: mess)
-            alerts.append(al)
+        for alert in alertsArray {
+            if let type = alert["type"] as? String,
+            let date = alert["date"] as? String,
+            let des = alert["description"] as? String,
+            let exp = alert["expires"] as? String,
+            let mes = alert["message"] as? String {
+                let alt = Alert(type: type, date: date, description: des, expires: exp, message: mes)
+                alerts.append(alt)
+            }
         }
         
         return alerts.count > 0 ? alerts : nil
@@ -46,24 +56,27 @@ class AlertsParser {
     
 }
 
-/* 
- 
- Example
- 
-*/
+// Example
+// ----------------------------------------------------------------------
 
-let jsonFile = NSBundle.mainBundle().pathForResource("alerts", ofType: "json")
-let jsonData = NSData(contentsOfFile: jsonFile!)
+let file = NSBundle.mainBundle().pathForResource("alerts", ofType: "json")
+let data = NSData(contentsOfFile: file!)
+
+let json: [String: AnyObject]?
+
+do {
+    json = try NSJSONSerialization.JSONObjectWithData(data!, options: .AllowFragments) as? [String: AnyObject]
+} catch let error as NSError {
+    json = nil
+    print("error is \(error.localizedDescription)")
+}
 
 let parser = AlertsParser()
-let alerts = parser.alertsFrom(jsonData)
+let alerts = parser.alertsFrom(json)
 
+alerts?.count
 alerts?[0].type
 alerts?[0].date
 alerts?[0].description
 alerts?[0].expires
 alerts?[0].message
-
-
-
-
