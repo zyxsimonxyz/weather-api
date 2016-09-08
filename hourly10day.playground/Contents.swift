74c273d94d689cb8d907c0a1988dabd86b7ca7da
wigging <wigging@me.com>
2016-09-07 23:58:53 -0400

    native swift approach

--- hourly10day.playground/Contents.swift
+++ hourly10day.playground/Contents.swift
@@ -1,17 +1,20 @@
 /*
-Hourly 10 day forecast from Weather Underground API.
 
-Parse the 10 day forecast conditions into a struct from the Weather Underground located at
-http://api.wunderground.com/api/#/hourly10day/q/knoxville,tn.json where # is your API key.
+Hourly 10 day forecast playground for parsing forecast data from the Weather
+Underground API.
 
-See the hourly10day.json file located in the Resources folder of this Playground for a local copy of
-the 10 day hourly conditions json data.
+See the hourly10day.json file located in the Resources folder of this
+Playground for a local copy of the json data.
+
+Weather Underground API documentation:
+https://www.wunderground.com/weather/api/d/docs
+
 */
 
 import UIKit
 
 // Model
-// ----------------------------------------------------------------------
+// -----------------------------------------------------------------------------
 
 struct Hourly {
     let yday: String
@@ -23,33 +26,31 @@ struct Hourly {
 // Parser
 // ----------------------------------------------------------------------
 
-class Parser {
+class HourlyParser {
     
     let units = 0
     
     /**
-     Parse the hourly 10 day forecast data from Weather Underground.
-     - Parameter data: The NSData from the Weather Underground API.
-     - Returns: Array of Hourly structs for next 240 hours.
+     Parse the hourly 10 day forecast returned as json data from Weather Underground.
+     - Parameter json: Dictionary representing json data.
+     - Returns: Array of hourly structs.
      */
     
-    func hourlyForecast(fromData: NSData?) -> [Hourly]? {
-        guard let data = fromData else { return nil }
-        let json = JSON(data: data)
+    func hourlyForecastFrom(json: [String: AnyObject]?) -> [Hourly]? {
+        
+        guard let hourlyArray = json?["hourly_forecast"] as? [[String: AnyObject]] else { return nil }
         
         var hourly = [Hourly]()
+        let keyUnits = units == 0 ? "english" : "metric"
         
-        for (_, item):(String, JSON) in json["hourly_forecast"] {
-            guard let yd = item["FCTTIME"]["yday"].string else {return nil}
-            guard let hr = item["FCTTIME"]["hour"].string else {return nil}
-            
-            let keyUnits = units == 0 ? "english" : "metric"
-            guard let tp = item["temp"][keyUnits].string else {return nil}
-            
-            guard let pp = item["pop"].string else {return nil}
-            
-            let hrly = Hourly(yday: yd, hourcivil: hr, temp: tp, pop: pp)
-            hourly.append(hrly)
+        for hour in hourlyArray {
+            if let yd = hour["FCTTIME"]?["yday"] as? String,
+            let hr = hour["FCTTIME"]?["hour"] as? String,
+            let tp = hour["temp"]?[keyUnits] as? String,
+            let pp = hour["pop"] as? String {
+                let hrly = Hourly(yday: yd, hourcivil: hr, temp: tp, pop: pp)
+                hourly.append(hrly)
+            }
         }
         
         return hourly.count > 0 ? hourly : nil
@@ -60,15 +61,23 @@ class Parser {
 // Example
 // ----------------------------------------------------------------------
 
-let jsonFile = NSBundle.mainBundle().pathForResource("hourly10day", ofType: "json")
-let jsonData = NSData(contentsOfFile: jsonFile!)
+let file = NSBundle.mainBundle().pathForResource("hourly10day", ofType: "json")
+let data = NSData(contentsOfFile: file!)
 
-let hourlyParser = Parser()
+let json: [String: AnyObject]?
 
-let hoursText = hourlyParser.hourlyForecast(jsonData)
+do {
+    json = try NSJSONSerialization.JSONObjectWithData(data!, options: .AllowFragments) as? [String: AnyObject]
+} catch let error as NSError {
+    json = nil
+    print("error is \(error.localizedDescription)")
+}
 
-hoursText?[0].yday
-hoursText?[0].hourcivil
-hoursText?[0].temp
-hoursText?[0].pop
+let parser = HourlyParser()
+let hourly = parser.hourlyForecastFrom(json)
 
+hourly?.count
+hourly?[0].yday
+hourly?[0].hourcivil
+hourly?[0].temp
+hourly?[0].pop
