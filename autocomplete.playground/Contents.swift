a7c0f2e0eb66bd6c753182587327f2ad875c8549
wigging <wigging@me.com>
2016-09-09 22:12:25 -0400

    native swift approach

--- autocomplete.playground/Contents.swift
+++ autocomplete.playground/Contents.swift
@@ -1,17 +1,20 @@
 /*
-Example of parsing a list of locations returned as JSON from the AutoComplete API.
 
-Link to AutoComplete API documentation
-http://www.wunderground.com/weather/api/d/docs?d=autocomplete-api
+Autocomplete playground for parsing a list of locations returned as JSON from 
+the Autocomplete API provied by the Weather Underground.
+
+See the autocomplete.json file located in the Resources folder of this Playground
+for a local copy of the json data.
+
+Weather Underground API documentation:
+https://www.wunderground.com/weather/api/d/docs
+
 */
 
 import UIKit
 
-/*
- 
- Model
- 
-*/
+// Model
+// -----------------------------------------------------------------------------
 
 struct Location {
     let name: String
@@ -20,28 +23,34 @@ struct Location {
     let longitude: String
 }
 
-/*
- 
- Parser
- 
-*/
+// Parser
+// -----------------------------------------------------------------------------
 
 class LocationsParser {
     
-    func locationsFrom(data: NSData?) -> [Location]? {
+    /**
+     Parse locations returned as json data from Autocomplete API.
+     - Parameter json: Dictionary representing json data.
+     - Returns: Array of location structs.
+     */
+    
+    func locationsFrom(json: [String: AnyObject]?) -> [Location]? {
         
-        guard let data = data else { return nil }
-        let json = JSON(data: data)
+        guard let json = json else { return nil }
+        guard let locationsArray = json["RESULTS"] as? [[String: AnyObject]] else { return nil }
         
         var locations = [Location]()
         
-        for (_, item):(String, JSON) in json["RESULTS"] {
-            guard let name = item["name"].string else { return nil }
-            guard let tzs = item["tzs"].string else { return nil }
-            guard let lat = item["lat"].string else { return nil }
-            guard let lon = item["lon"].string else { return nil }
-            let loc = Location(name: name, timezone: tzs, latitude: lat, longitude: lon)
-            locations.append(loc)
+        for loc in locationsArray {
+            if let
+                name = loc["name"] as? String,
+                tz = loc["tzs"] as? String,
+                lat = loc["lat"] as? String,
+                lon = loc["lon"] as? String
+            {
+                let lc = Location(name: name, timezone: tz, latitude: lat, longitude: lon)
+                locations.append(lc)
+            }
         }
         
         return locations.count > 0 ? locations : nil
@@ -49,18 +58,23 @@ class LocationsParser {
     
 }
 
-/*
- 
- Example
- 
-*/
+// Example
+// -----------------------------------------------------------------------------
 
-let path = NSBundle.mainBundle().pathForResource("autocomplete", ofType: "json")
-let jsonData = NSData(contentsOfFile: path!)
+let file = NSBundle.mainBundle().pathForResource("autocomplete", ofType: "json")
+let data = NSData(contentsOfFile: file!)
+
+let json: [String: AnyObject]?
+
+do {
+    json = try NSJSONSerialization.JSONObjectWithData(data!, options: .AllowFragments) as? [String: AnyObject]
+} catch let error as NSError {
+    json = nil
+    print("error is \(error.localizedDescription)")
+}
 
 let parser = LocationsParser()
-
-let locations = parser.locationsFrom(jsonData)
+let locations = parser.locationsFrom(json)
 
 locations?[0].name
 locations?[0].timezone
