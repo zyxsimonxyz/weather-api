05d9e08f7dda70d1f74218d8f36b32da2b9f3646
wigging <wigging@me.com>
2016-10-23 21:53:41 -0400

    use failable init for struct models

--- astronomy.playground/Contents.swift
+++ astronomy.playground/Contents.swift
@@ -13,7 +13,7 @@ https://www.wunderground.com/weather/api/d/docs
 
 import UIKit
 
-// Model
+// Models
 // -----------------------------------------------------------------------------
 
 struct Moon {
@@ -23,6 +23,32 @@ struct Moon {
     let hemisphere: String
 }
 
+extension Moon {
+    
+    /// Initialize Model model from JSON data.
+    /// - parameter json: JSON data
+    
+    init?(json: [String: Any]) {
+        
+        // extract dictionary from json data
+        guard let moonphase = json["moon_phase"] as? [String: Any] else { return nil }
+        
+        // extract values from dictionary
+        guard let perc = moonphase["percentIlluminated"] as? String else { return nil }
+        guard let age = moonphase["ageOfMoon"] as? String else { return nil }
+        guard let phase = moonphase["phaseofMoon"] as? String else { return nil }
+        guard let hemi = moonphase["hemisphere"] as? String else { return nil }
+        
+        // set struct properties
+        self.percentIlluminated = perc
+        self.age = age
+        self.phase = phase
+        self.hemisphere = hemi
+    }
+    
+}
+
+
 struct Sun {
     let riseHour: String
     let riseMinute: String
@@ -30,52 +56,27 @@ struct Sun {
     let setMinute: String
 }
 
-// Parser
-// -----------------------------------------------------------------------------
-
-class MoonSunParser {
+extension Sun {
     
-    /**
-     Parse moon phase returned as json data from Weather Underground API.
-     - Parameter json: Dictionary representing json data.
-     - Returns: A single moon struct.
-     */
-    
-    func moonFrom(json: [String: Any]?) -> Moon? {
+    init?(json: [String: Any]) {
         
-        guard let json = json else { return nil }
-        guard let moonphase = json["moon_phase"] as? [String: Any] else { return nil }
-        
-        guard let perc = moonphase["percentIlluminated"] as? String else { return nil }
-        guard let age = moonphase["ageOfMoon"] as? String else { return nil }
-        guard let phase = moonphase["phaseofMoon"] as? String else { return nil }
-        guard let hemi = moonphase["hemisphere"] as? String else { return nil }
-        
-        return Moon(percentIlluminated: perc, age: age, phase: phase, hemisphere: hemi)
-    }
-    
-    /**
-     Parse sun phase returned as json data from Weather Underground API.
-     - Parameter json: Dictionary representing json data.
-     - Returns: A single sun struct.
-     */
-    
-    func sunFrom(json: [String: Any]?) -> Sun? {
-        
-        guard let json = json else { return nil }
+        // extract dictionaries from json data
         guard let sunphase = json["sun_phase"] as? [String: Any] else { return nil }
         guard let sunrise = sunphase["sunrise"] as? [String: Any] else { return nil }
         guard let sunset = sunphase["sunset"] as? [String: Any] else { return nil }
         
-        
+        // extract values from dictionaries
         guard let risehr = sunrise["hour"] as? String else { return nil }
         guard let risemin = sunrise["minute"] as? String else { return nil }
         guard let sethr = sunset["hour"] as? String else { return nil }
         guard let setmin = sunset["minute"] as? String else { return nil }
         
-        return Sun(riseHour: risehr, riseMinute: risemin, setHour: sethr, setMinute: setmin)
+        // set struct values
+        self.riseHour = risehr
+        self.riseMinute = risemin
+        self.setHour = sethr
+        self.setMinute = setmin
     }
-    
 }
 
 // Example
@@ -93,18 +94,15 @@ do {
     print("Error is \(error.localizedDescription)")
 }
 
-let parser = MoonSunParser()
-
-let moon = parser.moonFrom(json: json!)
-
+let moon = Moon(json: json!)
 moon?.percentIlluminated
 moon?.age
 moon?.phase
 moon?.hemisphere
 
-let sun = parser.sunFrom(json: json!)
-
+let sun = Sun(json: json!)
 sun?.riseHour
 sun?.riseMinute
 sun?.setHour
 sun?.setMinute
+
